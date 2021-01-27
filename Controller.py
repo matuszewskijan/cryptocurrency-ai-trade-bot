9043665568026eac0deb692439d30c42d3a139cc
Jan Matuszewski <jmatuszewski@prograils.com>
2021-01-27 09:43:45 +0100

    Add missing arguments and imports to Controller

--- Controller.py
+++ Controller.py
@@ -1,39 +1,15 @@
 from Dataset import Dataset
 from Model import Model
 from Config import *
-from AutoTrader import AutoTrader
 import argparse
+import datetime
+
+from AutoTrader import AutoTrader
 from CoinbaseAPI import CoinbaseAPI
 
+import matplotlib.pyplot as plt
+
 if __name__ == '__main__':
-
-    print("\n\n\n                   ,.=ctE55ttt553tzs.,                           \n",
-          "              ,,c5;z==!!::::  .::7:==it3>.,                      \n",
-          "           ,xC;z!::::::    ::::::::::::!=c33x,                   \n",
-          "         ,czz!:::::  ::;;..===:..:::   ::::!ct3.                 \n",
-          "       ,C;/.:: :  ;=c!:::::::::::::::..      !tt3.               \n",
-          "      /z/.:   :;z!:::::J  :E3.  E:::::::..     !ct3.             \n",
-          "    ,E;F   ::;t::::::::J  :E3.  E::.     ::.     \ztL            \n",
-          "   ;E7.    :c::::F******   **.  *==c;..    ::     Jttk           \n",
-          "  .EJ.    ;::::::L                    \:.   ::.    Jttl          \n",
-          "  [:.    :::::::::773.    JE773zs.     I:. ::::.    It3L         \n",
-          " ;:[     L:::::::::::L    |t::!::J     |::::::::    :Et3         \n",
-          " [:L    !::::::::::::L    |t::;z2F    .Et:::.:::.  ::[13  CRYPTO \n",
-          " E:.    !::::::::::::L               =Et::::::::!  ::|13  TRADING \n",
-          " E:.    (::::::::::::L    .......       \:::::::!  ::|i3  AI      \n",
-          " [:L    !::::      ::L    |3t::::!3.     ]::::::.  ::[13          \n",
-          " !:(     .:::::    ::L    |t::::::3L     |:::::; ::::EE3         \n",
-          "  E3.    :::::::::;z5.    Jz;;;z=F.     :E:::::.::::II3[         \n",
-          "  Jt1.    :::::::[                    ;z5::::;.::::;3t3          \n",
-          "   \z1.::::::::::l......   ..   ;.=ct5::::::/.::::;Et3L          \n",
-          "    \z3.:::::::::::::::J  :E3.  Et::::::::;!:::::;5E3L           \n",
-          "     \cz\.:::::::::::::J   E3.  E:::::::z!     ;Zz37`            \n",
-          "       \z3.       ::;:::::::::::::::;='      ./355F              \n",
-          "         \z3x.         ::~======='         ,c253F                \n",
-          "           \ z3=.                      ..c5t32^                  \n",
-          "              =zz3==...          ...=t3z13P^                     \n",
-          "                   `*=zjzczIIII3zzztE3>*^`                        \n\n\n")
-
 
     print("\n> Welcome to Crypto-Trading AI")
 
@@ -43,6 +19,7 @@ if __name__ == '__main__':
     parser.add_argument("--trade", action="store_true")
     parser.add_argument("--start")
     parser.add_argument("--end")
+    parser.add_argument("--months")
     parser.add_argument("--pair")
     parser.add_argument("--interval")
     args = parser.parse_args()
