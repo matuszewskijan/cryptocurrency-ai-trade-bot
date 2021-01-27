fde6f08da410b217d68e4498f134f1e7a2179b4c
Jan Matuszewski <jmatuszewski@prograils.com>
2021-01-27 09:48:58 +0100

    Add Transactions array  and other parameters to VirtualAccount

--- VirtualAccount.py
+++ VirtualAccount.py
@@ -5,5 +5,7 @@ class VirtualAccount:
         self.btc_amount = 0
         self.btc_balance = 0
         self.btc_price = 0
-        self.bought_btc_at = 0
+        self.transactions = []
+        self.bought_btc_price = 0
+        self.bought_btc_units = 0
         self.last_transaction_was_sell = False;
